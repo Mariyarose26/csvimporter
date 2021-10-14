7415a32fc41cbdbcf94e3e9c4f154041e7c1dcf7
Mariya Rose <mariel.jesm@gmail.com>
2021-10-14 12:28:44 +1100

    Fix Comments

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const request = require("request");
 const fileToImport = "https://docs.google.com/spreadsheets/d/1JVqK-Nfa4jety-HE_KjrwwAwhGH6u1jsUVsSVbrxPxg/export?format=csv&gid=837598930";
 const dbUrl = "mongodb://localhost:27017/shopDB";
 
-// Declare the Order model
+// Declare the models
 const Order = require('./models/order.js');
 const Customer = require("./models/customer.js");
 
-// mongoose.Promise = global.Promise;
-// mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
 
 const csvImporter = async (fileToImport) => {
-  await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("MongoDB connected ...");
   });
 
